511aa24fb04f79f2eab4e3e458224109bc0473e0
Rafael-Bla <rafaelandrettabueno@gmail.com>
2026-08-27 20:58:23 -0300

    Update Planet.cs

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,7 +4,7 @@ public class Planet : MonoBehaviour
 {
     public string nomePlaneta;
     public float raio = 2f;
-    public float massa = 5f; // Já existia
+    public float massa = 5f; 
 
     [Header("Informações para o Satélite/Tabela")]
     public float forcaGravitacional;
@@ -12,6 +12,7 @@ public class Planet : MonoBehaviour
     public string tipoVida; // Ex: "Nenhuma", "Microbiana", "Inteligente"
     public bool temSeresInteligentes;
 
-    [Header("Exploração")]
-    public bool explorado = false; // Marca se já foi explorado
+    [Header("Exploração e Satélite")]
+    public bool explorado = false; 
+    public bool sateliteLancado = false; // <--- NOVA FLAG: impede nova mensagem
 }
